072d2bf0f719181902f1659e72c00b8750ebb18f
samuelaidoo45 <samuelaidoo45@gmail.com>
2021-06-02 21:59:52 +0000

    town json files update

--- controller/getData.js
+++ controller/getData.js
@@ -5,13 +5,13 @@ hd = new header();
 
 var datapath = './ghdata/regions/'
 
-
 const getData = (req, res, next) =>
 {
     res.header(hd.allowOrigin.key, hd.allowOrigin.value );
     res.header(hd.allowHeadrs.key,hd.allowHeadrs.value );
     res.header(hd.allowMethods.key,hd.allowMethods.value);
     res.header(hd.contentType.key, hd.contentType.value);
+    //hd.head(res);
     console.log(req.url);
     console.log(datapath + req.url+'.json');
     fs.readFile(datapath + req.url+'.json', "utf8",function(err,data){
--- dataObject/Header.js
+++ dataObject/Header.js
@@ -3,6 +3,13 @@ class Header{
      allowHeadrs = {key:"Access-Control-Allow-Headers",value:"Origin, X-Requested-With, Content-Type, Accept"};
      allowMethods = {key:"Access-Control-Allow-Methods",value:"PUT,POST,GET,DELETE,OPTIONS"};
      contentType = {key:"Content-Type",value:"application/json;charset=utf-8"};
+
+      head = function(res){
+          res.header(allowOrigin.key, allowOrigin.value );
+          res.header(allowHeadrs.key,allowHeadrs.value );
+          res.header(allowMethods.key,allowMethods.value);
+          res.header(contentType.key, contentType.value);
+      };
 }
 
 module.exports = Header;
