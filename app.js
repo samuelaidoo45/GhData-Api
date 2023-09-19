cf7976b15e939d64fca93a3d6fde25e8c3485eb7
SamA <45714641+samuelaidoo45@users.noreply.github.com>
2023-09-19 16:59:33 +0000

    update

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const universitiesRoute = require('./routes/universitiesRoute');
 const mpsRoute = require('./routes/mpsRoute');
 const path = require('path');
 
-app.use(express.static(__dirname + '/doc'));
+app.use(express.static('/home/tivateo2/app.tivateonline.com/ghdata/doc'));
+
+// app.use(express.static(__dirname + '/doc'));
 //const http = require('http');
 const fs = require('fs');
 
@@ -17,12 +19,11 @@ app.use('/', townsRoute);//connecting to route
 app.use('/', universitiesRoute);
 app.use('/', mpsRoute);
 
-app.use(express.static('/home/tivateo2/app.tivateonline.com/ghdata/'));
 
-// Serve the index.html for any route
-app.get('*', (req, res) => {
-  res.sendFile(path.join('/home/tivateo2/app.tivateonline.com/ghdata/', 'index.html'));
-});
+// // Serve the index.html for any route
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join('/home/tivateo2/app.tivateonline.com/ghdata/', 'index.html'));
+// });
 
 // app.get('/ghdata', function(req, res) { 
 //     res.sendFile('index.html',{root: __dirname});
