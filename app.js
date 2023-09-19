2d1d8e798e825c2ea71b8ea72db45fc44e0e5168
SamA <45714641+samuelaidoo45@users.noreply.github.com>
2023-09-19 22:54:09 +0000

    update

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ app.set('view engine', 'pug');
 
 app.use(express.json());
 
-app.use('/ghdata', townsRoute);//connecting to route
-app.use('/ghdata', universitiesRoute);
-app.use('/ghdata', mpsRoute);
+app.use('/ghdata/doc/', townsRoute);//connecting to route
+app.use('/ghdata/doc/', universitiesRoute);
+app.use('/ghdata/doc/', mpsRoute);
 
 
 // // Serve the index.html for any route
@@ -25,7 +25,7 @@ app.use('/ghdata', mpsRoute);
 //   res.sendFile(path.join('/home/tivateo2/app.tivateonline.com/ghdata/doc', 'index.html'));
 // });
 
-app.get('/ghdata', function(req, res) { 
+app.get('/ghdata/doc/', function(req, res) { 
     res.sendFile('index.html',{root: '/home/tivateo2/app.tivateonline.com/ghdata/doc/'});
 });//rendering index page
 
