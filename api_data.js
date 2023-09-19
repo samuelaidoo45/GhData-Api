faab87c4c7ac4b37c6f5cc75d26a4b0a0a45f5b2
SamA <45714641+samuelaidoo45@users.noreply.github.com>
2023-09-19 23:04:52 +0000

    update

---
+++ api_data.js
@@ -0,0 +1,285 @@
+define({ "api": [
+  {
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "optional": false,
+            "field": "varname1",
+            "description": "<p>No type.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "varname2",
+            "description": "<p>With type.</p>"
+          }
+        ]
+      }
+    },
+    "type": "",
+    "url": "",
+    "version": "0.0.0",
+    "filename": "./doc/main.js",
+    "group": "C:\\Users\\Eugene\\Projects\\MyProjects\\Api's\\Rest Api's\\GhData\\doc\\main.js",
+    "groupTitle": "C:\\Users\\Eugene\\Projects\\MyProjects\\Api's\\Rest Api's\\GhData\\doc\\main.js",
+    "name": ""
+  },
+  {
+    "type": "get",
+    "url": "/mps",
+    "title": "Members Of Parliament",
+    "name": "MembersOfParliament",
+    "group": "MP's",
+    "version": "0.0.0",
+    "filename": "./routes/mpsRoute.js",
+    "groupTitle": "MP's",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:3000/mps"
+      }
+    ]
+  },
+  {
+    "type": "get",
+    "url": "/ahafo-towns",
+    "title": "Towns in the Ahafo Region",
+    "name": "AhafoTowns",
+    "group": "Towns",
+    "version": "0.0.0",
+    "filename": "./routes/townsRoute.js",
+    "groupTitle": "Towns",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:3000/ahafo-towns"
+      }
+    ]
+  },
+  {
+    "type": "get",
+    "url": "/ashanti-towns",
+    "title": "Towns in the Ashanti Region",
+    "name": "AshantiTowns",
+    "group": "Towns",
+    "version": "0.0.0",
+    "filename": "./routes/townsRoute.js",
+    "groupTitle": "Towns",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:3000/ashanti-towns"
+      }
+    ]
+  },
+  {
+    "type": "get",
+    "url": "/bonoeast-towns",
+    "title": "Towns in the Bono East Region",
+    "name": "BonoEastTowns",
+    "group": "Towns",
+    "version": "0.0.0",
+    "filename": "./routes/townsRoute.js",
+    "groupTitle": "Towns",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:3000/bonoeast-towns"
+      }
+    ]
+  },
+  {
+    "type": "get",
+    "url": "/bono-towns",
+    "title": "Towns in the Bono Region",
+    "name": "BonoTowns",
+    "group": "Towns",
+    "version": "0.0.0",
+    "filename": "./routes/townsRoute.js",
+    "groupTitle": "Towns",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:3000/bono-towns"
+      }
+    ]
+  },
+  {
+    "type": "get",
+    "url": "/central-towns",
+    "title": "Towns in the Central Region",
+    "name": "CentralTowns",
+    "group": "Towns",
+    "version": "0.0.0",
+    "filename": "./routes/townsRoute.js",
+    "groupTitle": "Towns",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:3000/central-towns"
+      }
+    ]
+  },
+  {
+    "type": "get",
+    "url": "/eastern-towns",
+    "title": "Towns in the Eastern Region",
+    "name": "EasternTowns",
+    "group": "Towns",
+    "version": "0.0.0",
+    "filename": "./routes/townsRoute.js",
+    "groupTitle": "Towns",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:3000/eastern-towns"
+      }
+    ]
+  },
+  {
+    "type": "get",
+    "url": "/northeast-towns",
+    "title": "Towns in the North East Region",
+    "name": "NorthEastTowns",
+    "group": "Towns",
+    "version": "0.0.0",
+    "filename": "./routes/townsRoute.js",
+    "groupTitle": "Towns",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:3000/northeast-towns"
+      }
+    ]
+  },
+  {
+    "type": "get",
+    "url": "/northen-towns",
+    "title": "Towns in the Northern Region",
+    "name": "NothernTowns",
+    "group": "Towns",
+    "version": "0.0.0",
+    "filename": "./routes/townsRoute.js",
+    "groupTitle": "Towns",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:3000/northen-towns"
+      }
+    ]
+  },
+  {
+    "type": "get",
+    "url": "/oti-towns",
+    "title": "Towns in the Oti Region",
+    "name": "OtiTowns",
+    "group": "Towns",
+    "version": "0.0.0",
+    "filename": "./routes/townsRoute.js",
+    "groupTitle": "Towns",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:3000/oti-towns"
+      }
+    ]
+  },
+  {
+    "type": "get",
+    "url": "/savannah-towns",
+    "title": "Towns in the Savannah Region",
+    "name": "SavannahTowns",
+    "group": "Towns",
+    "version": "0.0.0",
+    "filename": "./routes/townsRoute.js",
+    "groupTitle": "Towns",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:3000/savannah-towns"
+      }
+    ]
+  },
+  {
+    "type": "get",
+    "url": "/uppereast-towns",
+    "title": "Towns in the Upper East Region",
+    "name": "UpperEastTowns",
+    "group": "Towns",
+    "version": "0.0.0",
+    "filename": "./routes/townsRoute.js",
+    "groupTitle": "Towns",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:3000/uppereast-towns"
+      }
+    ]
+  },
+  {
+    "type": "get",
+    "url": "/upperwest-towns",
+    "title": "Towns in the Upper West Region",
+    "name": "UpperWestTowns",
+    "group": "Towns",
+    "version": "0.0.0",
+    "filename": "./routes/townsRoute.js",
+    "groupTitle": "Towns",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:3000/upperwest-towns"
+      }
+    ]
+  },
+  {
+    "type": "get",
+    "url": "/volta-towns",
+    "title": "Towns in the Volta Region",
+    "name": "VoltaTowns",
+    "group": "Towns",
+    "version": "0.0.0",
+    "filename": "./routes/townsRoute.js",
+    "groupTitle": "Towns",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:3000/volta-towns"
+      }
+    ]
+  },
+  {
+    "type": "get",
+    "url": "/westernnorth-towns",
+    "title": "Towns in the Western North Region",
+    "name": "WesternMpthTowns",
+    "group": "Towns",
+    "version": "0.0.0",
+    "filename": "./routes/townsRoute.js",
+    "groupTitle": "Towns",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:3000/westernnorth-towns"
+      }
+    ]
+  },
+  {
+    "type": "get",
+    "url": "/western-towns",
+    "title": "Towns in the Western Region",
+    "name": "WesternTowns",
+    "group": "Towns",
+    "version": "0.0.0",
+    "filename": "./routes/townsRoute.js",
+    "groupTitle": "Towns",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:3000/western-towns"
+      }
+    ]
+  },
+  {
+    "type": "get",
+    "url": "/universities",
+    "title": "Universities in Ghana",
+    "name": "universitiesInGhana",
+    "group": "Universities",
+    "version": "0.0.0",
+    "filename": "./routes/universitiesRoute.js",
+    "groupTitle": "Universities",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:3000/universities"
+      }
+    ]
+  }
+] });
--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 // app.use(express.static('/home/tivateo2/app.tivateonline.com/ghdata/doc'));
 
-app.use(express.static('/home/tivateo2/app.tivateonline.com/ghdata/' + '/doc/'));
+app.use(express.static('/home/tivateo2/app.tivateonline.com/ghdata/'));
 //const http = require('http');
 const fs = require('fs');
 
@@ -15,9 +15,9 @@ app.set('view engine', 'pug');
 
 app.use(express.json());
 
-app.use('/ghdata/doc/', townsRoute);//connecting to route
-app.use('/ghdata/doc/', universitiesRoute);
-app.use('/ghdata/doc/', mpsRoute);
+app.use('/ghdata/', townsRoute);//connecting to route
+app.use('/ghdata/', universitiesRoute);
+app.use('/ghdata/', mpsRoute);
 
 
 // // Serve the index.html for any route
@@ -25,8 +25,8 @@ app.use('/ghdata/doc/', mpsRoute);
 //   res.sendFile(path.join('/home/tivateo2/app.tivateonline.com/ghdata/doc', 'index.html'));
 // });
 
-app.get('/ghdata/doc/', function(req, res) { 
-    res.sendFile('index.html',{root: '/home/tivateo2/app.tivateonline.com/ghdata/doc/'});
+app.get('/ghdata/', function(req, res) { 
+    res.sendFile('index.html',{root: '/home/tivateo2/app.tivateonline.com/ghdata/'});
 });//rendering index page
 
 // LISTENING TO SERVER
